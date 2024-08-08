e0803cdada643eacea1df56a15aaf945a62c44ee
Naveen Yadav <yn.naveen00@gmail.com>
2024-08-08 13:44:01 +0530

    navbar Toggle set

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,34 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Button from './Button'
 import Link from 'next/link'
+import clsx from 'clsx'
 
 const Navbar = () => {
+    const [ariaexpanded, setAriaexpanded] = useState(false);
+    const handleClick = ()=>{
+        if(ariaexpanded===false)
+            setAriaexpanded(true);
+        else
+            setAriaexpanded(false)
+    }
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="/logo_new.webp" className="h-8" alt="Flowbite Logo" />
-                    
                 </Link>
-                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded={ariaexpanded} onClick={handleClick}>
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                     </svg>
                 </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                <div className={clsx({
+                    "hidden w-full md:block md:w-auto": ariaexpanded === false,
+                    "w-full md:block md:w-auto": ariaexpanded === true
+                })}
+                    id="navbar-default">
                     <ul className="font-medium flex flex-col justify-center items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <Link href="/about" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">About</Link>
@@ -30,7 +42,7 @@ const Navbar = () => {
                         <li>
                             <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
                         </li>
-                       
+
                         <li>
                             <Link href='/' className='flex flex-col justify-center items-center'>
                                 <Button input='Login/Register' color='' />
@@ -39,7 +51,7 @@ const Navbar = () => {
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav >
 
     )
 }
